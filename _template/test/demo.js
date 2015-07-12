ee7c16fd26a4b1b514f6613ed088271782caee14
valleykid <valleykiddy@gmail.com>
2015-07-12 08:10:45 +0800

    fix bug

--- _template/test/demo.js
+++ _template/test/demo.js
@@ -9,14 +9,16 @@
 	iCat.loader.require('jquery, underscore, backbone, <%=name%>', function(){
 		var v = new iCat.widget.<%=name%>({
 			el: '.J_widgetBox',
+			//style: false, // Don't use the default
+			//dcname: 'test', // --way1: chunk-data render
 			events: {
 				'click.insert .item': 'insert'
 				//,'click.cc .item': 'insert'
 			},
 			mconfig: {
-				//api: {testUrl: 'test/data.json'},
-				//syncfunArgus: {a:1, b:2},
-				MergeData: {
+				//api: {testUrl: 'test/data.json'}, // --way2: ajax-data render
+				//afArgus: {a:1, b:2},
+				MergeData: { // --way3: user-data render
 					results: [
 						{name:1}, {name:2}, {name:3},
 						{name:4}, {name:5}, {name:6}
